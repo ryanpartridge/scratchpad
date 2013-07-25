30c55ce309ec590d7f20e376472c62e04a0c0283
rpartridge <rpartridge@control4.com>
2013-07-25 16:30:51 -0600

    starting up

--- thread_interrupt/thread_interrupt.cpp
+++ thread_interrupt/thread_interrupt.cpp
@@ -4,7 +4,19 @@
  *  Created on: Jul 25, 2013
  *      Author: rpartridge
  */
+#include <iostream>
+#include <boost/cstdint.hpp>
 
+void wasteTimeNoInterrupt()
+{
+    boost::uint64_t val = 0;
+}
+
+int main(int argc, char* argv[])
+{
+    wasteTimeNoInterrupt();
+    return 0;
+}
 
 
 
