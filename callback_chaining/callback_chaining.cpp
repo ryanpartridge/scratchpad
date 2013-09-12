23935af02374df838f3bc10c2dcf2deb83fc6bbf
ryanpartridge <ryan.partridge@gmail.com>
2013-09-12 16:08:18 -0600

    working callback chaining

--- callback_chaining/callback_chaining.cpp
+++ callback_chaining/callback_chaining.cpp
@@ -14,12 +14,31 @@
 #include <boost/function.hpp>
 #include <boost/asio.hpp>
 
-typedef boost::function<void(int, std::string)> LowLevelCallback;
+typedef boost::function<void(int, const std::string)> LowLevelCallback;
+typedef boost::function<void(const std::string, bool)> HighLevelCallback;
+
+void kicker(LowLevelCallback llc)
+{
+    llc(1, "hello");
+}
+
+void lowLevelFunc(int intVal, const std::string strVal, HighLevelCallback hlc)
+{
+    std::cout << "[lowlevelFunc] intVal: " << intVal << " strVal: " << strVal << std::endl;
+    hlc(strVal, true);
+}
+
+void highLevelFunc(const std::string strVal, bool boolVal)
+{
+    std::cout << "[highLevelFunc] strVal: " << strVal << " boolVal: " << boolVal << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
     boost::asio::io_service io_service;
     std::cout << "[main] calling run" << std::endl;
+    kicker(boost::bind(lowLevelFunc, _1, _2, highLevelFunc));
+
     io_service.run();
     std::cout << "[main] run complete" << std::endl;
     return 0;
