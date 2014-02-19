e5c57c91620ca28f9e910a279c7fa4e70e837976
ryanpartridge <ryan.partridge@gmail.com>
2014-02-19 10:40:18 -0700

    timer expiration testing

--- timer_expire/timer_expire.cpp
+++ timer_expire/timer_expire.cpp
@@ -1,92 +1,54 @@
 /*
- * function_pointer.cpp
+ * timer_expire.cpp
  *
- *  Created on: Jan 24, 2014
+ *  Created on: Feb 19, 2014
  *      Author: rpartridge
  */
 
 #include <iostream>
-#include <boost/function.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/chrono.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/thread.hpp>
 #include <boost/bind.hpp>
+#include <boost/ref.hpp>
 
 using namespace std;
 
-typedef boost::function<void(void)> voidVoidFunc;
-typedef boost::function<void(int)> voidIntFunc;
-
-void func1(int anInt)
+void handler(const boost::system::error_code& ec)
 {
-    cout << "func1: " << anInt << endl;
+    if (!ec)
+    {
+        cout << "timer expired" << endl;
+    }
+    else if (ec == boost::asio::error::operation_aborted)
+    {
+        cout << "timer canceled" << endl;
+    }
 }
 
 int main(int argc, char* argv[])
 {
     cout << "entering main" << endl;
-    voidIntFunc bound1;
-    cout << "trying bound1 before initializing" << endl;
-    if (bound1)
-    {
-        bound1(1);
-    }
-    else
-    {
-        cout << "did not execute" << endl;
-    }
 
-    bound1 = voidIntFunc(func1);
-    cout << "trying bound1 after initializing" << endl;
-    if (bound1)
-    {
-        bound1(1);
-    }
-    else
-    {
-        cout << "did not execute" << endl;
-    }
+    boost::asio::io_service io_service;
+    boost::shared_ptr<boost::asio::steady_timer> timer = boost::make_shared<boost::asio::steady_timer>(boost::ref(io_service), boost::chrono::seconds(10));
+    timer->async_wait(&handler);
+    boost::shared_ptr<boost::thread> timer_thread = boost::make_shared<boost::thread>(boost::bind(&boost::asio::io_service::run, boost::ref(io_service)));
 
-    bound1.clear();
-    cout << "trying bound1 after clearing" << endl;
-    if (bound1)
+    boost::this_thread::sleep_for(boost::chrono::seconds(2));
+    if (timer->expires_from_now() > boost::chrono::seconds(1))
     {
-        bound1(1);
+        cout << "timer has not yet expired" << endl;
     }
-    else
-    {
-        cout << "did not execute" << endl;
-    }
+    boost::this_thread::sleep_for(boost::chrono::seconds(2));
+    cout << "resetting timer" << endl;
+    timer->expires_from_now(boost::chrono::seconds(2));
+    timer->async_wait(&handler);
 
-    bound1 = boost::bind(&func1, _1);
-    cout << "trying bound1 after binding without value" << endl;
-    if (bound1)
-    {
-        bound1(1);
-    }
-    else
-    {
-        cout << "did not execute" << endl;
-    }
-
-    bound1 = boost::bind(&func1, 7);
-    cout << "trying bound1 after binding with value" << endl;
-    if (bound1)
-    {
-        bound1(1);
-    }
-    else
-    {
-        cout << "did not execute" << endl;
-    }
-
-    voidVoidFunc bound2(boost::bind(&func1, 1));
-    cout << "trying bound2 after binding with value" << endl;
-    if (bound2)
-    {
-        bound2();
-    }
-    else
-    {
-        cout << "did not execute" << endl;
-    }
+    timer_thread->join();
 
     cout << "exiting main" << endl;
     return 0;
