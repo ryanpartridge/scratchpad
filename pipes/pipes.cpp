8c471d23b7887be472d0cd5f53fa155eb9afe61e
ryanpartridge <ryan.partridge@gmail.com>
2019-06-06 16:12:02 -0600

    more pipes

--- pipes/pipes.cpp
+++ pipes/pipes.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/ref.hpp>
+
+void myfunc(boost::asio::io_context& io_context)
+{
+    std::cout << "Running myfunc" << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
     std::cout << "Hello world!" << std::endl;
+    boost::asio::io_context io_context;
+
+    io_context.post(boost::bind(&myfunc, boost::ref(io_context)));
+    io_context.run();
+    std::cout << "All done!" << std::endl;
 }
