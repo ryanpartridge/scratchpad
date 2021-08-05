58227b7398554b32c782de10709f3e955a3491a5
ryanpartridge <ryan.partridge@gmail.com>
2021-08-05 14:58:25 -0600

    better example implementation

--- async_to_sync/async_to_sync.cpp
+++ async_to_sync/async_to_sync.cpp
@@ -4,49 +4,64 @@
 #include <chrono>
 
 #include <boost/asio.hpp>
+#include <boost/asio/spawn.hpp>
 
-void handleTimer(boost::system::error_code const& ec, std::shared_ptr<boost::asio::steady_timer> timer)
+class Runner :
+    public std::enable_shared_from_this<Runner>
 {
-    if (ec)
-    {
-        std::cerr << "timer error: " << ec.message() << std::endl;
-    }
-    else
-    {
-        std::cout << "timer expired" << std::endl;
-    }
-    timer.reset();
-}
+public:
+    explicit Runner(boost::asio::io_context& io_context) :
+        io_context_(io_context)
+    {}
 
-void eventLoop(boost::asio::io_context& io_context)
-{
-    std::cout << "event loop starting" << std::endl;
+    ~Runner() = default;
 
-    //auto timer = std::make_shared<boost::asio::steady_timer>(io_context, std::chrono::seconds(2));
+    void start(boost::asio::yield_context yield)
+    {
+        std::cout << "staring runner" << std::endl;
+        timer_ = std::make_shared<boost::asio::steady_timer>(io_context_, std::chrono::seconds(2));
+
 /*
-    timer->async_wait(
-        [timer](boost::system::error_code const& ec)
-        {
-            if (ec)
+        timer_->async_wait(
+            [](boost::system::error_code const& ec)
             {
-                std::cerr << "timer error: " << ec.message() << std::endl;
+                if (ec)
+                {
+                    std::cerr << "timer error: " << ec.message() << std::endl;
+                }
+                else
+                {
+                    std::cout << "timer expired" << std::endl;
+                }
             }
-            else
-            {
-                std::cout << "timer expired" << std::endl;
-            }
-        }
-    );
+        );
 */
 
-    std::cout << "event loop finished" << std::endl;
-}
+        boost::system::error_code ec;
+        timer_->async_wait(yield[ec]);
+        if (ec)
+        {
+            std::cerr << "timer error: " << ec.message() << std::endl;
+        }
+        else
+        {
+            std::cout << "timer expired" << std::endl;
+        }
+
+        std::cout << "leaving the start function" << std::endl;
+    }
+
+private:
+    boost::asio::io_context& io_context_;
+    std::shared_ptr<boost::asio::steady_timer> timer_;
+};
 
 int main(int argc, char* argv[])
 {
     std::cout << "async_to_sync startup" << std::endl;
     boost::asio::io_context io_context;
-    boost::asio::post(std::bind(&eventLoop, std::ref(io_context)));
+    auto runner = std::make_shared<Runner>(io_context);
+    boost::asio::spawn(std::bind(&Runner::start, runner, std::placeholders::_1));
     io_context.run();
     std::cout << "async_to_sync complete" << std::endl;
     return 0;
