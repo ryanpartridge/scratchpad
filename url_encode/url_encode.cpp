da16c04c6bbd7807bbc7bbe59d21fbf3f18d6fc9
ryanpartridge <ryan.partridge@gmail.com>
2021-08-25 09:10:44 -0600

    finish decode method

--- url_encode/url_encode.cpp
+++ url_encode/url_encode.cpp
@@ -9,11 +9,51 @@ std::string encode(std::string const& input)
 
 std::string decode(std::string const& input)
 {
-    std::string decoded(input);
-    for (auto c: decoded)
+    auto index = size_t(0);
+    auto lastIndex = index;
+    auto length = input.size();
+    std::string decoded;
+
+    while (index < length)
     {
-        std::cout << c << std::endl;
+        index = input.find('%', lastIndex);
+        if (index != std::string::npos)
+        {
+            decoded.append(input, lastIndex, index - lastIndex);
+            ++index;
+            if ((index + 2) < length)
+            {
+                auto value = input.substr(index, 2);
+                try
+                {
+                    size_t badIndex = 0;
+                    int code = std::stoi(value, &badIndex, 16);
+                    if (badIndex == value.size())
+                    {
+                        decoded.append(1, code);
+                    }
+                    else
+                    {
+                        return decoded;
+                    }
+                }
+                catch(std::exception const& ex)
+                {
+                    return decoded;
+                }
+                lastIndex = index + 2;
+            }
+            else
+            {
+                return decoded;
+            }
+        }
+        else
+        {
+            decoded.append(input, lastIndex);
+        }
     }
+
     return decoded;
 }
 
@@ -21,9 +61,14 @@ int main(int argc, char* argv[])
 {
     std::cout << "url_encode program" << std::endl;
 
-    std::string encoded("url%20encoded");
+    std::string encoded{"url%20encoded%22string"};
     std::cout << "encoded: " << encoded << std::endl;
     std::cout << "decoded: " << decode(encoded) << std::endl;
+    std::cout << std::endl;
+
+    std::string unencoded{"url encoded*string"};
+    std::cout << "unencoded: " << unencoded << std::endl;
+    std::cout << "encoded: " << encode(unencoded) << std::endl;
 
 /*
     uint32_t c{' '};
