9ac6e75d7090849d24c43e41d222ef5242cf5d76
ryanpartridge <ryan.partridge@gmail.com>
2022-02-17 13:59:01 -0700

    more of the move_test

--- move_test/move_test.cpp
+++ move_test/move_test.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <tuple>
+#include <string>
 
-int main(uint32_t argc, char* argv[])
+using my_tuple = std::tuple<uint32_t, std::string>;
+
+void receiveByMove(my_tuple&& value)
+{
+    std::cout << "int: " << std::get<0>(value) << std::endl << "string: " << std::get<1>(value) << std::endl << std::endl;
+}
+
+my_tuple returnByMove(uint32_t intVal, std::string strVal)
+{
+    // let the compiler employ return value optimization
+    my_tuple t(intVal, strVal);
+    return t;
+}
+
+int main(int argc, char* argv[])
 {
     std::cout << "starting move_test" << std::endl;
+
+    my_tuple t(0, std::string("hello world"));
+    receiveByMove(std::move(t));
+    t = returnByMove(1, std::string("hello again"));
+
     return 0;
 }
