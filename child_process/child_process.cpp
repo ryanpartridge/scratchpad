57acc8e2e9153350078caa535cf65d26cae971ba
ryanpartridge <ryan.partridge@gmail.com>
2023-02-23 20:34:39 -0700

    leverage life-cycle memory management

--- child_process/child_process.cpp
+++ child_process/child_process.cpp
@@ -19,21 +19,22 @@
 
 using ProcessExitCallback = std::function<void(int32_t, std::error_code const&)>;
 
-class AptAsyncHandler :
+class AsyncProcessHandler :
     public boost::process::extend::async_handler
 {
 public:
-    AptAsyncHandler() = default;
+    AsyncProcessHandler() = default;
 
-    AptAsyncHandler(AptAsyncHandler const&) = delete;
+    AsyncProcessHandler(AsyncProcessHandler const&) = delete;
 
-    explicit AptAsyncHandler(ProcessExitCallback callback) :
+    explicit AsyncProcessHandler(ProcessExitCallback callback) :
         callback_(callback)
     {}
 
     template<typename Exec>
     ProcessExitCallback on_exit_handler(Exec& /* exec */)
     {
+        std::cout << "grabbing the exit handler" << std::endl;
         auto callback = this->callback_;
         return [callback](int32_t exitCode, std::error_code const& ec)
         {
@@ -41,7 +42,10 @@ public:
         };
     }
 
-    ~AptAsyncHandler() = default;
+    virtual ~AsyncProcessHandler()
+    {
+        std::cout << "AsyncProcessHandler destructor" << std::endl;
+    }
 
 private:
     ProcessExitCallback callback_;
@@ -51,70 +55,42 @@ class ProcessRunner :
     public std::enable_shared_from_this<ProcessRunner>
 {
 public:
-    ProcessRunner() = default;
+    ProcessRunner() = delete;
     ProcessRunner(ProcessRunner const&) = delete;
+
+    explicit ProcessRunner(boost::asio::io_context& io_context) :
+        io_context_{io_context}
+    {}
 
     virtual ~ProcessRunner()
     {
-    }
-
-    auto start() -> void
-    {
-        io_context_.run();
+        std::cout << "ProcessRunner destructor" << std::endl;
     }
 
     auto asyncRunProcess() -> void
     {
         std::cout << "running command: \"" << cmd << "\"" << std::endl;
-        ProcessExitCallback callback = std::bind(&ProcessRunner::exitCallback, shared_from_this(), std::placeholders::_1, std::placeholders::_2);
-        asyncHandler_ = std::make_unique<AptAsyncHandler>(callback);
-        childProcess_ = std::make_unique<boost::process::child>("/usr/bin/sleep", "5", *asyncHandler_, boost::process::std_out > boost::process::null, boost::process::std_err > boost::process::null, io_context_);
-        //childProcess_ = std::make_unique<boost::process::child>(cmd, *asyncHandler_, boost::process::std_out > boost::process::null, boost::process::std_err > boost::process::null, io_context_);
-        timestamp_ = std::chrono::steady_clock::now();
+        auto processTimer = std::make_shared<boost::asio::steady_timer>(io_context_, std::chrono::seconds(13));
+        ProcessExitCallback callback = std::bind(&ProcessRunner::processComplete, shared_from_this(), std::placeholders::_1, std::placeholders::_2, processTimer);
+        AsyncProcessHandler asyncHandler{callback};
+        auto childProcess = std::make_shared<boost::process::child>(cmd, asyncHandler, boost::process::std_out > boost::process::null, boost::process::std_err > boost::process::null, io_context_);
 
-        auto instance = shared_from_this();
-        processTimer_ = std::make_unique<boost::asio::steady_timer>(io_context_, std::chrono::seconds(120));
         std::cout << "starting timer" << std::endl;
-        processTimer_->async_wait(
-            [instance](boost::system::error_code const& timer_ec)
+        auto timestamp = std::chrono::steady_clock::now();
+        processTimer->async_wait(
+            [self = shared_from_this(), timestamp, childProcess, callback](boost::system::error_code const& timer_ec)
             {
-                if (timer_ec)
-                {
-                    std::cout << "timer error (" << timer_ec.value() << "): " << timer_ec.message() << std::endl;
-                }
-                else
-                {
-                    std::cout << "timer expired";
-                    auto elapsed = std::chrono::steady_clock::now() - instance->timestamp_;
-                    std::cout << " (" << std::chrono::duration_cast<std::chrono::seconds>(elapsed).count() << " seconds elapsed)";
-                    if (elapsed > std::chrono::minutes(2))
-                    {
-                        if (instance->childProcess_)
-                        {
-                            std::cout << " -- killing child process";
-                            std::error_code processEc;
-                            instance->childProcess_->terminate(processEc);
-                        }
-                    }
-                    else
-                    {
-                        auto remaining = std::chrono::minutes(2) - elapsed;
-                        std::cout << " -- not enough time passed (" << std::chrono::duration_cast<std::chrono::seconds>(remaining + std::chrono::seconds(5)).count() << " remaining)";
-                    }
-                    std::cout << std::endl;
-                }
-                instance->processTimer_.reset();
+                self->processTimerComplete(timer_ec, timestamp, childProcess, callback);
             }
         );
     }
 
-    auto exitCallback(int32_t exitCode, std::error_code const& ec) -> void
+    auto processComplete(int32_t exitCode, std::error_code const& ec, std::shared_ptr<boost::asio::steady_timer> processTimer) -> void
     {
-        if (processTimer_)
+        if (processTimer)
         {
-            processTimer_->cancel();
+            processTimer->cancel();
         }
-        childProcess_.reset();
         std::cout << "process exit code: " << exitCode << std::endl;
         if (ec)
         {
@@ -122,20 +98,64 @@ public:
         }
     }
 
+    auto processTimerComplete(boost::system::error_code const& ec, std::chrono::steady_clock::time_point timestamp, std::shared_ptr<boost::process::child> childProcess, ProcessExitCallback exitCallback) -> void
+    {
+        if (ec)
+        {
+            if (ec == boost::asio::error::operation_aborted)
+            {
+                std::cout << "process timer canceled" << std::endl;
+            }
+            else
+            {
+                std::cerr << "process timer error (" << ec.value() << "): " << ec.message() << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "process timer expired" << std::endl;
+            auto elapsed = std::chrono::steady_clock::now() - timestamp;
+            std::cout << "time elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count() << " milliseconds" << std::endl;
+            if (childProcess && childProcess->valid())
+            {
+                std::error_code processEc;
+                if (childProcess->running(processEc))
+                {
+                    std::cout << "killing child process" << std::endl;
+                    processEc = {};
+                    childProcess->terminate(processEc);
+                }
+                else
+                {
+                    std::cerr << "timer expired but the process is not runnning???" << std::endl;
+                    std::cout << "invoking process callback directly" << std::endl;
+                    boost::asio::post(io_context_,
+                        [self = shared_from_this(), exitCallback]()
+                        {
+                            if (exitCallback)
+                            {
+                                exitCallback(0, {});
+                            }
+                        }
+                    );
+                }
+            }
+        }
+    }
+
 private:
-    const std::string cmd{"/usr/bin/sleep 125"};
-    boost::asio::io_context io_context_;
-    std::chrono::steady_clock::time_point timestamp_;
-    std::unique_ptr<boost::process::child> childProcess_;
-    std::unique_ptr<AptAsyncHandler> asyncHandler_;
-    std::unique_ptr<boost::asio::steady_timer> processTimer_;
+    const std::string cmd{"/usr/bin/sleep 5"};
+    boost::asio::io_context& io_context_;
 };
 
 int main(int argc, char* argv[])
 {
     std::cout << "child_process program" << std::endl;
-    auto runner = std::make_shared<ProcessRunner>();
-    runner->asyncRunProcess();
-    runner->start();
+    boost::asio::io_context io_context;
+    auto runner = std::make_shared<ProcessRunner>(io_context);
+    boost::asio::post(io_context, [runner]() { runner->asyncRunProcess(); });
+    std::cout << "staring the io_context" << std::endl;
+    io_context.run();
+    std::cout << "io_context complete" << std::endl;
     return 0;
 }
