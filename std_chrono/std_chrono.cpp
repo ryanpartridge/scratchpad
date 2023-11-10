faad0d09839b47bb96ea4b0b8887d3d2412f7b6d
ryanpartridge <ryan.partridge@gmail.com>
2023-11-09 21:00:23 -0700

    time_point code

--- std_chrono/std_chrono.cpp
+++ std_chrono/std_chrono.cpp
@@ -19,6 +19,41 @@ int main(int argc, char* argv[])
     std::cout << "Target time (string): " << std::ctime(&targetTimeT);
     std::cout << "Target time has " << (targetTimePoint < std::chrono::system_clock::now() ? "" : "NOT ") << "expired" << std::endl;
 
+    std::chrono::steady_clock::time_point const defaultTP;
+    std::chrono::steady_clock::time_point const minTP{std::chrono::steady_clock::time_point::min()};
+    std::chrono::steady_clock::time_point const maxTP{std::chrono::steady_clock::time_point::max()};
+
+    if (defaultTP == minTP)
+    {
+        std::cout << "default is the same as min" << std::endl;
+    }
+    else if (defaultTP > minTP)
+    {
+        std::cout << "default is greater than min" << std::endl;
+    }
+    else
+    {
+        std::cout << "default is less than min" << std::endl;
+    }
+
+    if (defaultTP == maxTP)
+    {
+        std::cout << "default is the same as max" << std::endl;
+    }
+    else if (defaultTP > maxTP)
+    {
+        std::cout << "default is greater than max" << std::endl;
+    }
+    else
+    {
+        std::cout << "default is less than max" << std::endl;
+    }
+
+    auto offsetTP = defaultTP + std::chrono::hours(48);
+    std::cout << "offset is greater than default: " << std::boolalpha << (offsetTP > defaultTP) << std::endl;
+    offsetTP = {};
+    std::cout << "offset is the same as default: " << std::boolalpha << (offsetTP == defaultTP) << std::endl;
+
     std::cout << "exiting main" << std::endl;
     return 0;
 }
