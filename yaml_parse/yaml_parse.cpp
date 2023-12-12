4e85ac8af03d212617e04de585b5745fa1635814
ryanpartridge <ryan.partridge@gmail.com>
2023-12-12 16:31:24 -0700

    more yaml parsing

--- yaml_parse/yaml_parse.cpp
+++ yaml_parse/yaml_parse.cpp
@@ -5,6 +5,22 @@
 #include <ryml.hpp>
 #include <ryml_std.hpp>
 
+void walkNode(ryml::ConstNodeRef node)
+{
+    if (node.has_key())
+    {
+        std::cout << "node: " << node.key() << " (" << (node.has_val() ? node.val() : "[no value]") << ")" << std::endl;
+    }
+    if (node.is_container())
+    {
+        std::cout << "node is container (" << (node.is_seq() ? "sequence" : (node.is_map() ? "map" : "unknown")) << ")" << std::endl;
+        for (auto child: node.children())
+        {
+            walkNode(child);
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
     std::cout << "staring yaml_parse" << std::endl;
@@ -23,9 +39,8 @@ int main(int argc, char* argv[])
     const auto restrictionsPayload = payloadStream.str();
     restrictionsFile.close();
 
-    ryml::Tree tree = ryml::parse_in_arena(ryml::to_csubstr(restrictionsPayload));
-        
+    auto tree = ryml::parse_in_arena(ryml::to_csubstr(restrictionsPayload));
+    walkNode(tree.crootref());
     std::cout << "exiting yaml_parse" << std::endl;
     return 0;
 }
-
